2486872375f2ce4d53e25e0952fab48c420d6bf5
cristians-12 <cristian.sanchez.casd@gmail.com>
2024-10-04 22:20:53 -0500

    backend trabajando en controlador de usuarios

--- backendVue/app/Http/Controllers/NoteController.php
+++ backendVue/app/Http/Controllers/NoteController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Note;
 use Illuminate\Http\Request;
 
 class NoteController extends Controller
 {
-    //
+    public function index() {
+        $notes = Note::all();
+        return response()->json($notes);
+    }
 }
--- backendVue/routes/api.php
+++ backendVue/routes/api.php
@@ -1,14 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
+use Illuminate\Support\Facades\Route;
 
 Route::controller(UserController::class)->group(function () {
     Route::get('/users', 'index');
 });
-
