6c2015a8b115874492914c7509e15d4682e047d9
cristians-12 <cristian.sanchez.casd@gmail.com>
2024-10-04 23:08:54 -0500

    Users crud success, add notes crud

--- backendVue/app/Http/Controllers/UserController.php
+++ backendVue/app/Http/Controllers/UserController.php
@@ -11,4 +11,39 @@ class UserController extends Controller
     {
         return User::all();
     }
+
+    public function getUser($id)
+    {
+        $user = User::find($id);
+        if ($user) return response()->json(['success' => true, 'data' => $user], 202);
+        return response()->json(['message' => 'No users found', 'success' => false], 404);
+    }
+
+    public function createUser(Request $request)
+    {
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = bcrypt($request->password);
+        $user->save();
+        return response()->json(['message' => 'User saved']);
+    }
+
+    public function updateUser(Request $request, $id)
+    {
+        $user = User::find($id);
+        if (!$user) {
+            return response()->json(['message' => 'User not found', 'success' => false], 404);
+        }
+
+        if ($request->name && $request->password && $request->name) {
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->password = bcrypt($request->password);
+            $user->save();
+            return response()->json(['message' => 'User updated',], 202);
+        } else {
+            return response()->json(['message' => 'All inputs must be filled']);
+        }
+    }
 }
--- backendVue/routes/api.php
+++ backendVue/routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
 use App\Http\Controllers\UserController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::controller(UserController::class)->group(function () {
     Route::get('/users', 'index');
+    Route::get('/users/{id}', 'getUser');
+    Route::post('/users', 'createUser');
+    Route::put('/users/{id}', 'updateUser');
 });
