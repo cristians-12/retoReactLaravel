d87fe3665dd77a4f620f0e5a707840f81e30e797
cristians-12 <cristian.sanchez.casd@gmail.com>
2024-11-15 22:13:57 -0500

    add login frontend

--- frontend/src/components/AddNoteModal.vue
+++ frontend/src/components/AddNoteModal.vue
@@ -1,25 +1,27 @@
 <template>
+  <div
+    class="backdrop-blur-md bg-orange-800 text-white flex flex-col gap-5 rounded-lg p-5"
+  >
+    <div class="">
+      <input
+        class="w-full px-5 p-2 rounded-xl"
+        type="text"
+        placeholder="Añadir nombre nota"
+      />
+    </div>
+    <div>
+      <p>Ingrese la descripcion de la nota o tarea:</p>
+      <!-- <input
+        class="w-60 border border-gray-700 px-5 p-2 rounded-xl"
+        type="text"
+        placeholder="Añadir descripcion"
+      /> -->
+    </div>
+    <textarea></textarea>
     <div
-        class="backdrop-blur-md bg-yellow-600 flex flex-col gap-5 rounded-lg p-5"
-      >
-        <div>
-          <input
-            class="w-60 border border-gray-700 px-5 p-2 rounded-xl"
-            type="text"
-            placeholder="Añadir nombre nota"
-          />
-        </div>
-        <div>
-          <input
-            class="w-60 border border-gray-700 px-5 p-2 rounded-xl"
-            type="text"
-            placeholder="Añadir descripcion"
-          />
-        </div>
-        <div
-          class="bg-orange-600 rounded-xl px-5 text-center text-white py-2 cursor-pointer hover:scale-105"
-        >
-          Guardar nota
-        </div>
-      </div>
+      class="bg-orange-600 rounded-xl px-5 text-center text-white py-2 cursor-pointer hover:scale-105"
+    >
+      Guardar nota
+    </div>
+  </div>
 </template>
